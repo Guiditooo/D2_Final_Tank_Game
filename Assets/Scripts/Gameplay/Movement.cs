ccfcfb86e620bf8234ca538cbb0f53da818ecb54
Guido Tello <lpmaleinn@gmail.com>
2023-02-26 23:55:54 -0300

    Added Managers

---
+++ Assets/Scripts/Gameplay/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    [SerializeField] private float rotationSpeed = 1.0f;
+    [SerializeField] private float movementSpeed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
