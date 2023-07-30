62d44fa8b0d2ce1ad72dd1d5ed4055385bdc9724
Guido Tello <lpmaleinn@gmail.com>
2023-07-30 08:33:49 -0300

    Update UIController

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,7 +1,8 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
-
+using System.Collections.Generic;
+using System.Collections;
 
 public class UIController : MonoBehaviour
 {
@@ -10,20 +11,29 @@ public class UIController : MonoBehaviour
 
     [Header("Menu scene")]
     [SerializeField] private string menuSceneName = "";
-
     [SerializeField] private TMP_Text timerText = null;
+    
+    [Header("Gameplay")]
+    [SerializeField] private string gameplaySceneName = "";
 
-    public static System.Action OnGameOverButtonClick;
+    [Header("Game Over Section")]
+    [SerializeField] private TMP_Text bombsPoints = null;
+    [SerializeField] private TMP_Text timerPoints = null;
+    [SerializeField] private TMP_Text scorePoints = null;
+    [SerializeField] private CanvasGroup summaryPanel = null;
+    [SerializeField] private CanvasGroup hiScorePanel = null;
 
     private void Awake()
     {
         PauseSystem.OnPauseStateChange += PausePanelController;
         GameManager.OnTimerChange += UpdateTimerText;
+        GameManager.OnGameOver += LoadGameOver;
     }
     private void OnDestroy()
     {
         PauseSystem.OnPauseStateChange -= PausePanelController;
         GameManager.OnTimerChange -= UpdateTimerText;
+        GameManager.OnGameOver -= LoadGameOver;
     }
     private void PausePanelController(PauseStates state)
     {
@@ -36,13 +46,13 @@ public class UIController : MonoBehaviour
             ShowPanel(pausePanel);
         }
     }
-    private void ShowPanel(CanvasGroup panel)
+    public void ShowPanel(CanvasGroup panel)
     {
         panel.alpha = 1;
         panel.blocksRaycasts = true;
         panel.interactable = true;
     }
-    private void HidePanel(CanvasGroup panel)
+    public void HidePanel(CanvasGroup panel)
     {
         panel.blocksRaycasts = false;
         panel.interactable = false;
@@ -58,9 +68,40 @@ public class UIController : MonoBehaviour
         timerText.text = newValue.ToString();
     }
 
-    public void LoadGameOverScene()
+    private void CalculateScores()
     {
-        OnGameOverButtonClick();
+        IEnumerator ShowTimeScore, ShowBombScore, ShowTotalScore;
+        ShowTimeScore = ShowScore(timerPoints, GameManager.TimeScore, 1.5f);
+        ShowBombScore = ShowScore(bombsPoints, GameManager.BombScore, 1.5f);
+        ShowTotalScore = ShowScore(scorePoints, GameManager.TotalScore, 1.5f);
+
+        StartCoroutine(ShowTimeScore);
+        StartCoroutine(ShowBombScore);
+        StartCoroutine(ShowTotalScore);
+    }
+    IEnumerator ShowScore(TMP_Text text, int value, float timeToShow)
+    {
+        float scorePerFrame = value / timeToShow;
+        float shownValue = 0;
+        float showingTime = 0;
+        while (showingTime<timeToShow)
+        {
+            showingTime += Time.deltaTime;
+            shownValue += scorePerFrame;
+            text.text = shownValue.ToString();
+        yield return null;
+        }
+        text.text = value.ToString();
+    }
+
+    private void LoadGameOver()
+    {
+        ShowPanel(summaryPanel);
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene();
     }
 
 }
