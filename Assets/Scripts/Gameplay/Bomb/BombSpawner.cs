985e09a054a8ed578d1c50cdd375a4e3352ff60e
Guido Tello <lpmaleinn@gmail.com>
2023-07-30 04:58:42 -0300

    Create Bomb Spawner Script

---
+++ Assets/Scripts/Gameplay/Bomb/BombSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject bombPrefab;
+
+
+
+    private int bombCount;
+    private void Awake()
+    {
+        //Fijarme cuantas bombas hay
+        //Crear las bombas totales
+    }
+
+
+}
