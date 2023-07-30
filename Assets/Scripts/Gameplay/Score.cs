d8ac0d96183df36da5790d5ef117323beac3b710
Guido Tello <lpmaleinn@gmail.com>
2023-07-30 04:59:39 -0300

    Create Score Script

---
+++ Assets/Scripts/Gameplay/Score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private bool isSelected = false;
+
+    private UnityEngine.UI.Image img = null;
+
+    private void Awake()
+    {
+        img = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    private void Update()
+    {
+        if(isSelected)
+        {
+            img.color = new Color(Random.Range(0,1.0f), Random.Range(0, 1.0f), Random.Range(0, 1.0f), 0.15f);
+        }
+    }
+
+}
