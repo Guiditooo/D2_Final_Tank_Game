c900c23435bfcad30e54cc1574870c2824268d04
Guido Tello <lpmaleinn@gmail.com>
2023-08-11 06:21:47 -0300

    Updated ParamConfigurator
    Its the menu configuration for the amount of time and bombs.

--- Assets/Scripts/Utils/ParamConfigurator.cs
+++ Assets/Scripts/Utils/ParamConfigurator.cs
@@ -9,10 +9,20 @@ namespace GT
     public class ParamConfigurator : MonoBehaviour
     {
         private Dictionary<string, int> parameters = new Dictionary<string, int>();
+
+        private DataManager dataManager = null;
+
+        private void Start()
+        {
+            dataManager = DataManager.Instance;
+        }
+
         public void SaveParameters()
         {
+            int bombCount = 0;
+            int timeCount = 0;
             foreach (OptionValueChanger OVC in GetComponentsInChildren<OptionValueChanger>())
-            {
+            { //Esto es hermoso si no tengo que persistir la data con mi DataManager. Cambiar a futuro.
                 int value = int.MaxValue;
                 value = (int)((OVC.GetComponentInChildren<Slider>()).value);
                 if (parameters.ContainsKey(OVC.name))
@@ -23,7 +33,18 @@ namespace GT
                 {
                     parameters.Add(OVC.name, value);
                 }
+
+                if (OVC.name == "Time") //Esto es horrible :P
+                {
+                    timeCount = value;
+                }
+                else
+                {
+                    bombCount = value;
+                }
             }
+
+            dataManager.SetGameData(bombCount, timeCount);
 
             foreach (string key in parameters.Keys)
             {
